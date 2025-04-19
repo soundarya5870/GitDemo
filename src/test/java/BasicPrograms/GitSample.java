b9feb0380d61de599d6e9bef5bd3cff4aa007d14
soundarya <soundaryasuryawanshi1999@gmail.com>
2025-04-19 17:54:00 +0530

    For master Commit

--- src/test/java/BasicPrograms/GitSample.java
+++ src/test/java/BasicPrograms/GitSample.java
@@ -1,11 +1,16 @@
 package BasicPrograms;
 
-public class GitSample {
 
+public class GitSample {
+	public void master()
+	{
+		System.out.println("This is for master branch");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Hello World");
  System.out.println("This is dev branch");
+ 
 	}
 
 }
